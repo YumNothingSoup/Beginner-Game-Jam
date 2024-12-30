c70e3a03bd19cc55f216de98d5d21bcd29dbe946
WhimFreak <whimmyfreaky@gmail.com>
2024-12-31 03:46:53 +0800

    Add files via upload

--- UI/turn_label.gd
+++ UI/turn_label.gd
@@ -3,9 +3,10 @@ extends Label
 func _ready() -> void:
 	Events.player_turn_started.connect(display_turn_player)
 	Events.enemy_turn_started.connect(display_turn_enemy)
+	display_turn_player()
 	
 func display_turn_player():
-	text = "CURRENT TURN: PLAYER"
+	text = "PLAYER"
 	
 func display_turn_enemy():
-	text = "CURRENT TURN: PINS"
+	text = "PINS"
