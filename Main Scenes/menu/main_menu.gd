1332310a0ad1bf8f84fbc332af917db9ea80cab4
maxeDev <m.zeband@gmail.com>
2024-12-31 11:13:40 +0100

    add bg scene back to main menu and make scene changes working again

--- Main Scenes/menu/main_menu.gd
+++ Main Scenes/menu/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://scene_not_main/entry_to_game.tscn")
+	get_tree().change_scene_to_file("res://Main Scenes/menu/select_difficulty.tscn")
 
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main Scenes/settings.tscn")
---
+++ Main Scenes/menu/select_difficulty.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+func _on_easy_pressed() -> void:
+	change_scene_to_game()
+
+func _on_medium_pressed() -> void:
+	change_scene_to_game()
+
+func _on_hard_pressed() -> void:
+	change_scene_to_game()
+
+func change_scene_to_game() -> void:
+	get_tree().change_scene_to_file("res://scene_not_main/entry_to_game.tscn")
