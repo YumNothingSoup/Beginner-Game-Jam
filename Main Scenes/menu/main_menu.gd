b75abdf50f11f4d7f40be7f403e0e790e4424f56
WhimFreak <whimmyfreaky@gmail.com>
2025-01-02 13:26:04 +0800

    Add files via upload

--- Main Scenes/menu/main_menu.gd
+++ Main Scenes/menu/main_menu.gd
@@ -4,7 +4,7 @@ func _on_start_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main Scenes/menu/select_difficulty.tscn")
 
 func _on_settings_pressed() -> void:
-	get_tree().change_scene_to_file("res://Main Scenes/settings.tscn")
+	get_tree().change_scene_to_file("res://Main Scenes/menu/settings.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
